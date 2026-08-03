a971e2bf774c7ebf6b98b87402778bd3e6654c51
sr-0397 <148094877+sr-0397@users.noreply.github.com>
2026-08-02 23:26:35 -0400

    fix: delete scratch_repro.py

--- scratch_repro.py
+++
@@ -1,6 +0,0 @@
-# scratch_repro.py
-from safety.pii_scrubber import PIIScrubber
-
-s = PIIScrubber()
-print(repr(s.scrub("Call me at (555) 123-4567 or 555-123-4567")))
-print(s.detect("Call me at (555) 123-4567"))
