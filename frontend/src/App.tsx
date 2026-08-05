60a667ce808a6730fad9093c808c81ff3a1cbc9d
rose413 <116857309+rose413@users.noreply.github.com>
2026-08-04 18:54:03 -0700

    feat(frontend): add public /share/:token page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import { DashboardPage } from './pages/DashboardPage'
 import { NewProfilePage } from './pages/NewProfilePage'
 import { ReviewPage } from './pages/ReviewPage'
 import { ReviewHistoryPage } from './pages/ReviewHistoryPage'
+import { PublicReviewPage } from './pages/PublicReviewPage'
 
 const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { user, isLoading } = useAuth()
@@ -84,6 +85,7 @@ function App() {
             </ProtectedRoute>
           }
         />
+        <Route path="/share/:token" element={<PublicReviewPage />} />
       </Routes>
     </>
   )
